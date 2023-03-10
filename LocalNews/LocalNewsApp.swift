15dba9691755f079d3e71e5fc309ef8752ebf493
Awaleh m Hassan <awalehmhassan@mbp-de-awaleh.home>
2023-03-10 21:42:46 +0100

    inittial commit

---
+++ LocalNews/LocalNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNewsApp.swift
+//  LocalNews
+//
+//  Created by Awaleh m Hassan on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LocalNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
